5906001cebbb89595378e4e539763977231159ce
Vuong Truong <vuong.truong@mgisolutions.com>
2023-07-10 14:06:09 +0700

    resizeable

--- components/grid-table/index.vue
+++ components/grid-table/index.vue
@@ -1,107 +1,141 @@
 <template>
-    <div>
-        <HeaderToolbar :rows="rows" :columns="columns" v-model:visibleColumns="visibleColumns"></HeaderToolbar>
+  <div>
+    <HeaderToolbar
+      :rows="rows"
+      :columns="columns"
+      v-model:visibleColumns="visibleColumns"
+    ></HeaderToolbar>
+  </div>
+  <div class="q-pa-md">
+    <q-table
+      flat
+      bordered
+      v-table-resizable
+      :rows="rows"
+      :columns="columns"
+      :title="title"
+      :visible-columns="visibleColumns"
+      hide-pagination
+      separator="cell"
+      v-model:pagination="pagination"
+      selection="single"
+      v-model:selected="selected"
+      :filter="filter"
+      row-key="id"
+    >
+      <template v-slot:top-right>
+        <q-input
+          borderless
+          dense
+          debounce="300"
+          v-model="filter"
+          placeholder="Search"
+        >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+      <!-- <template v-slot:header="props">
+        <q-tr :props="props">
+          <q-th
+            v-for="col in props.cols"
+            :key="col.name"
+            :props="props"
+            class="text-purple font-bold text-2xl"
+          >
+            {{ col.label }}
+          </q-th>
+        </q-tr>
+      </template>
+      <template v-slot:body="props" v-if="$slots.body">
+        <q-tr :props="props">
+          <q-td key="name" :props="props">
+            {{ props.row.name }}
+          </q-td>
+        </q-tr>
+      </template> -->
+    </q-table>
+    <div class="flex justify-end">
+      <div class="flex min-w-[100px] align-center">
+        <q-select
+        transition-show="scale"
+        transition-hide="scale"
+        filled
+        v-model="model"
+        :options="options"
+        style="width: 50px"
+      />
+      </div>
+      <div class="flex justify-center">
+        <q-btn   v-if="pagesNumber > 2" @click="firstPage" icon="first_page" color="grey-8" round dense flat />
+
+        <q-btn
+          icon="chevron_left"
+          color="grey-8"
+          round
+          dense
+          flat
+          :disable="props.pagination.page <=1"
+          @click="prePage"
+        />
+
+        <q-btn
+          icon="chevron_right"
+          color="grey-8"
+          round
+          dense
+          flat
+          :disable="props.pagination.page >= pagesNumber"
+          @click="nextPage"
+        />
+
+        <q-btn   v-if="pagesNumber > 2" @click="lastPage" icon="last_page" color="grey-8" round dense flat />
+      </div>
     </div>
-    <div class="">
-        <q-table :rows="rows" :columns="columns" :title="title" :visible-columns="visibleColumns" hide-pagination
-            v-model:pagination="pagination" v-model:selected="selected" :filter="filter" bordered row-key="name">
-            <template v-slot:top-right>
-                <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
-                    <template v-slot:append>
-                        <q-icon name="search" />
-                    </template>
-                </q-input>
-            </template>
-            <template v-slot:header="props">
-                <q-tr :props="props">
-                    <q-th v-for="col in props.cols" :key="col.name" :props="props" class="text-purple font-bold text-2xl">
-                        {{ col.label }}
-                    </q-th>
-                </q-tr>
-            </template>
-            <template v-slot:body="props" v-if="$slots.body">
-                <q-tr :props="props">
-                    <q-td key="name" :props="props">
-                        {{ props.row.name }}
-                    </q-td>
-                </q-tr>
-            </template>
-        </q-table>
-        <div class="flex justify-center">
-            <div>
-                <q-btn color="primary" label="Basic Menu">
-                    <q-menu>
-                        <q-list style="min-width: 100px">
-                            <q-item clickable v-close-popup>
-                                <q-item-section>New tab</q-item-section>
-                            </q-item>
-                            <q-item clickable v-close-popup>
-                                <q-item-section>New incognito tab</q-item-section>
-                            </q-item>
-                            <q-separator />
-                            <q-item clickable v-close-popup>
-                                <q-item-section>Recent tabs</q-item-section>
-                            </q-item>
-                            <q-item clickable v-close-popup>
-                                <q-item-section>History</q-item-section>
-                            </q-item>
-                            <q-item clickable v-close-popup>
-                                <q-item-section>Downloads</q-item-section>
-                            </q-item>
-                            <q-separator />
-                            <q-item clickable v-close-popup>
-                                <q-item-section>Settings</q-item-section>
-                            </q-item>
-                            <q-separator />
-                            <q-item clickable v-close-popup>
-                                <q-item-section>Help &amp; Feedback</q-item-section>
-                            </q-item>
-                        </q-list>
-                    </q-menu>
-                </q-btn>
-
-            </div>
-            <q-btn icon="first_page" color="grey-8" round dense flat />
-
-            <q-btn icon="chevron_left" color="grey-8" round dense flat @click="prePage" />
-
-            <q-btn icon="chevron_right" color="grey-8" round dense flat @click="nextPage" />
-
-            <q-btn icon="last_page" color="grey-8" round dense flat />
-        </div>
-    </div>
+  </div>
 </template>
-  
-<script setup >
-import HeaderToolbar from './header-toolbar.vue';
+
+<script setup>
+import HeaderToolbar from "./header-toolbar.vue";
 
 const props = defineProps({
-    rows: Array,
-    columns: Array,
-    pagination: Object,
-    minWidth: Number,
-    title: String,
+  rows: Array,
+  columns: Array,
+  pagination: Object,
+  minWidth: Number,
+  title: String,
 });
-const emit = defineEmits(['prePage', 'nextpage']);
+const emit = defineEmits(["prePage", "nextpage", "pageSize", 'setPage']);
 
-const filter = ref('');
+const filter = ref("");
 const visibleColumns = ref(
-    props.columns.map((c) => (c.isShow ? c.name : null))
+  props.columns.map((c) => (c.isShow ? c.name : null))
 );
 const selected = ref([]);
 const pagination = ref(props.pagination);
 const pagesNumber = computed(() => {
-    return Math.ceil(pagination.value.rowsNumber / pagination.value.rowsPerPage)
+  return Math.ceil(props.pagination.rowsNumber / props.pagination.rowsPerPage);
 });
-console.log('length', props.rows)
-console.log('pagesNumber', pagesNumber.value)
-
+watch(pagesNumber, (val) => {
+  console.log(val)
+}, { immediate: true})
+const options = ref([3, 5, 10, 12, 15]);
+const model = ref(5);
 // Funtion
 const nextPage = () => {
-    emit('nextPage', props.pagination.page + 1,)
+  emit("setPage", props.pagination.page + 1);
 };
 const prePage = () => {
-    emit('prePage', props.pagination.page - 1,)
+  emit("setPage", props.pagination.page - 1);
 };
+const firstPage = () => {
+  emit("setPage", 1);
+};
+const lastPage = () => {
+  emit("setPage", pagesNumber.value);
+};
+watch(model, (val) => {
+  emit("pageSize", val);
+})
 </script>
-  
--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,102 @@
 <template>
-    <GridTable :rows="dataProducts?.products" :columns="columns" v-model:pagination="pagination" @nextPage="nextPage"
-        @prePage="prePage" title="Products table" />
+  <GridTable
+    :rows="dataProducts?.products"
+    :columns="columns"
+    v-model:pagination="pagination"
+    @pageSize="pageSize"
+    @setPage="setPage"
+    title="Products table"
+  />
 </template>
-  
+
 <script setup lang="ts">
-import { reactive, watch } from 'vue';
-import GridTable from '@/components/grid-table/index.vue';
+import { reactive, watch } from "vue";
+import GridTable from "@/components/grid-table/index.vue";
 
 const pagination = ref({
-    page: 1,
-    rowsPerPage: 5,
-    rowsNumber: 30,
+  page: 1,
+  rowsPerPage: 5,
+  rowsNumber: 100,
 });
 
-const { data: dataProducts } = await useAsyncData('products', () => $fetch('/api/products/view-home-page', {
-    query: {
+const { data: dataProducts } = await useAsyncData(
+  "products",
+  () =>
+    $fetch("/api/products/view-home-page", {
+      query: {
         page: pagination.value.page,
-        pageSize: pagination.value.rowsPerPage
-    }
-}), { watch: [pagination] })
+        pageSize: pagination.value.rowsPerPage,
+      },
+    }),
+  { watch: [pagination] }
+);
 
 const columns = reactive([
-    {
-        name: 'id',
-        label: 'id',
-        required: true,
-        align: 'left',
-        field: 'id',
-        format: val => `${val}`,
-        sortable: true,
-        isShow: true,
-        // style: 'width:80px',
-    },
-    {
-        name: 'price',
-        label: 'price',
-        field: (rows) => `${rows.price}$`,
-        sortable: true,
-        isShow: true,
-    },
-    {
-        name: 'title',
-        label: 'title',
-        field: 'title',
-        sortable: true,
-        isShow: true,
-    },
-    {
-        name: 'rating',
-        label: 'rating',
-        field: 'rating',
-        sortable: true,
-        isShow: true,
-    },
-    {
-        name: 'description',
-        label: 'description',
-        field: 'description',
-        isShow: false,
-    },
-    {
-        name: 'brand',
-        label: 'brand',
-        field: 'brand',
-        sortable: true,
-        isShow: true,
-    },
-    {
-        name: 'category',
-        label: 'category',
-        field: 'category',
-        sortable: true,
-        isShow: true,
-    },
+  {
+    name: "id",
+    label: "id",
+    required: true,
+    align: "left",
+    field: 'id',
+    format: (val) => `${val}`,
+    sortable: true,
+    isShow: true,
+  },
+  {
+    name: "price",
+    label: "price",
+    align:'center',
+    field: (rows) => `${rows.price}$`,
+    sortable: true,
+    isShow: true,
+  },
+  {
+    name: "title",
+    label: "title",
+    field: "title",
+    sortable: true,
+    isShow: true,
+  },
+  {
+    name: "rating",
+    label: "rating",
+    field: "rating",
+    sortable: true,
+    isShow: true,
+  },
+  {
+    name: "description",
+    label: "description",
+    field: "description",
+    isShow: false,
+  },
+  {
+    name: "brand",
+    label: "brand",
+    field: "brand",
+    sortable: true,
+    isShow: true,
+  },
+  {
+    name: "category",
+    label: "category",
+    field: "category",
+    sortable: true,
+    isShow: true,
+  },
 ]);
 
 // Function
-const nextPage = (ev) => {
-    pagination.value = {
-        ...pagination.value,
-        page: ev,
-    };
-    console.log(pagination.value)
+const setPage = (e) => {
+  pagination.value = {
+    ...pagination.value,
+    page: e,
+  };
 };
-const prePage = (ev) => {
-    pagination.value = {
-        ...pagination.value,
-        page: ev,
-    };
+const pageSize = (ev) => {
+  pagination.value = {
+    ...pagination.value,
+    rowsPerPage: ev,
+  };
 };
 </script>
